e4c3148219e69456ff282a92e7fedcca09ea29c8
Camilo Tobar <jc@swiset.com>
2019-07-24 10:05:47 -0500

    First commit

---
+++ src/components/Lista.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="lista">
+      Lista {{users.length}}
+    <ul>
+        <li v-bind:key="user.id" v-for="user in users">
+            {{ user.name }}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Lista',
+  props: { 
+      users: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="users">
+    <h1>Componente de Users</h1>
+    <Lista :users="users"></Lista>
+  </div>
+</template>
+
+<script>
+import Lista from './Lista'
+export default {
+  name: 'Users',
+  data: function(){
+      return {
+          users: [
+                { id: '1', name: 'Camilo' },
+                { id: '2', name: 'Juan' }
+            ]
+      }
+  },
+  components: 
+  {
+      Lista
+  },
+  props: { }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
